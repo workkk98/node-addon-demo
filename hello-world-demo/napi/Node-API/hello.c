7048513da509ee5ad67ed122a96aa9622e3638c6
zhanghefan <zhanghefan@bytedance.com>
2024-05-06 11:43:34 +0800

    addon & js性能

--- hello-world-demo/napi/Node-API/hello.c
+++ hello-world-demo/napi/Node-API/hello.c
@@ -2,18 +2,51 @@
 #include <assert.h>
 #include <node_api.h>
 
+static int fabonacci(int n) {
+  if (n == 1 || n == 2) {
+    return 1;
+  }
+  return fabonacci(n - 1) + fabonacci(n - 2);
+}
+
 // Hello方法的实现，该方法返回hello
 static napi_value Hello(napi_env env, napi_callback_info info) {
   napi_status status;
   napi_value world;
 
-  status = napi_create_string_utf8(env, "hello-world", 12, &world);
+  status = napi_create_string_utf8(env, "hello-zhfcc", 12, &world);
 
   assert(status == napi_ok);
 
   return world;
 }
 
+static napi_value Fabonacci(napi_env env, napi_callback_info info) {
+  napi_status status;
+  napi_value world;
+
+  int result = fabonacci(20);
+
+  status = napi_create_int32(env, result, &world);
+
+  assert(status == napi_ok);
+
+  return world;
+}
+
+// typedef struct {
+  // One of utf8name or name should be NULL.
+//   const char* utf8name;
+//   napi_value name;
+
+//   napi_callback method;
+//   napi_callback getter;
+//   napi_callback setter;
+//   napi_value value;
+
+//   napi_property_attributes attributes;
+//   void* data;
+// } napi_property_descriptor;
 
 #define DECLARE_NAPI_METHOD(name, func) \
   { name, 0, func, 0, 0, 0, napi_default, 0 }
@@ -23,7 +56,12 @@ static napi_value Init(napi_env env, napi_value exports) {
 
   napi_property_descriptor desc = DECLARE_NAPI_METHOD("hello", Hello); // 设置exports对象的描述结构体
 
-  status = napi_define_properties(env, exports, 1, &desc); // env表示底层上下文的参数
+  napi_property_descriptor desc2 = DECLARE_NAPI_METHOD("fabonacci", Fabonacci); // 设置exports对象的描述结构体
+  // napi_property_descriptor desc2;
+
+  napi_property_descriptor descs[] = {desc, desc2};
+
+  status = napi_define_properties(env, exports, 2, descs); // env表示底层上下文的参数
 
   assert(status == napi_ok);
 
--- hello-world-demo/napi/Node-API/test.js
+++ hello-world-demo/napi/Node-API/test.js
@@ -1,3 +1,17 @@
 let addon = require('bindings')('hello');
 
-console.log(addon.hello()); // 'hello world'
+console.time('addon fabonacci')
+addon.fabonacci()
+console.timeEnd('addon fabonacci')
+
+
+function fabonacci(a) {
+  if (a === 1 || a === 2) {
+    return a
+  }
+  return fabonacci(a - 1) + fabonacci(a - 2)
+}
+
+console.time('fabonacci')
+fabonacci(20)
+console.timeEnd('fabonacci')
